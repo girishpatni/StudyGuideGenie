766cf58a5b16949c2052d39a775177bef6f874f7
girishpatni <gpatni@asu.edu>
2016-04-07 19:37:31 -0700

    Template modified to display user notes and corrosponding changes in view

--- guide/views.py
+++ guide/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Notes
 
 # Create your views here.
 
 def Notes_list(request):
-	return render(request,'guide/Notes_list.html',{})
+	notes = Notes.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	return render(request,'guide/Notes_list.html',{'notes':notes})
 
