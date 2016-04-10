410696bbe13675757cb1449afa6ce4483d6e8b5f
girishpatni <gpatni@asu.edu>
2016-04-10 00:28:49 -0700

    Added view and template for notes detail and CSS

--- guide/urls.py
+++ guide/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Notes_list, name='Notes_list'),
+    url(r'^notes/(?P<pk>\d+)/$',views.Notes_detail,name='Notes_detail'),
 ]
--- guide/views.py
+++ guide/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Notes
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
 def Notes_list(request):
 	notes = Notes.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
 	return render(request,'guide/Notes_list.html',{'notes':notes})
+
+def Notes_detail(request, pk):
+	notes = get_object_or_404(Notes,pk=pk)
+	return render(request,'guide/Notes_detail.html',{'notes':notes})
+
 
